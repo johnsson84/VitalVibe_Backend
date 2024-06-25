d34edd65d77f0514119b664a72d42e65a3181d05
Johan Johnsson <johan@johnsson-net.se>
2024-06-25 11:11:30 +0200

    init

---
+++ src/main/java/com/backend/vitalvibe/VitalvibeApplication.java
@@ -0,0 +1,13 @@
+package com.backend.vitalvibe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitalvibeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitalvibeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/vitalvibe/VitalvibeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.vitalvibe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitalvibeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
