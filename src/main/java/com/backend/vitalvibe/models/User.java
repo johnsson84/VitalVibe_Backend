99a942492c51e4d33118106041dfe27be9adc667
Johan Johnsson <johan@johnsson-net.se>
2024-07-01 19:25:43 +0200

    models

---
+++ src/main/java/com/backend/vitalvibe/models/User.java
@@ -0,0 +1,83 @@
+package com.backend.vitalvibe.models;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Document(collection = "users")
+public class User {
+    @Id
+    private String id;
+
+    @NotBlank
+    @Size(max = 50)
+    private String username;
+
+    @NotBlank
+    @Email
+    @Indexed(unique = true)
+    private String email;
+
+    @NotBlank
+    @Size(max = 120)
+    private String password;
+
+    // role
+    @DBRef
+    private Set<Role> roles = new HashSet<>();
+    // ["ROLE_USER", "ROLE MODERATOR"]
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+}
